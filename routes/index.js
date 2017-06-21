4066d186c626b3533dda6b6d5994f96467a4d5b7
= <=>
2017-06-21 23:55:33 +0530

    onclick added

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,9 @@ router.get('/', function(req, res, next) {
 
 router.get('/home' , function(req,response,next){
    
-    var url = "http://api.railwayapi.com/name_number/train/bhopal/apikey/yqo4vcq8/";
+    // var url = "http://api.railwayapi.com/name_number/train/bhopal/apikey/yqo4vcq8/";
+    var url = "http://api.railwayapi.com/name_number/train/bhopal/apikey/i0eoy0jn/";
+    
     	var data = "";
 	     http.get(url, function(res){
 	         var body = '';
@@ -22,9 +24,15 @@ router.get('/home' , function(req,response,next){
 	         });
 
 	         res.on('end', function(){
-	              data = JSON.parse(body);
-	             console.log("Got a response: ",  data.train);
-	             response.render('home',data);
+	              data = JSON.parse(body); 
+
+	              if(data.response_code==200){
+	             console.log("Got a response: ",  data.response_code);
+	             response.render('home',data);}
+	             else
+	             {
+	             	response.send("Bad Gateway from aryan");
+	             }
 	         });
 
 	         
@@ -33,7 +41,6 @@ router.get('/home' , function(req,response,next){
 	     });
 
 	    
- console.log(data.train);
  
 });
 module.exports = router;
